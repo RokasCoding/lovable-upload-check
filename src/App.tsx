3881b4d8d93022a7f8ef186655741632986e8e22
RokasCoding <rpcskn@gmail.com>
2025-06-09 20:28:28 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { useEffect } from 'react';
 import { supabase } from '@/lib/supabase';
+// @ts-ignore
+window.supabase = supabase;
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
